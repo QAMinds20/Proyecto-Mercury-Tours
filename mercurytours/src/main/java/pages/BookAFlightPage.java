bac61f93ebf0dd8be68d06539c83afe53c16976c
Daniel Albavera <dalbavera@hotmail.com>
2020-05-16 13:21:00 -0500

    page BookAFlightPage created

--- mercurytours/src/main/java/pages/BookAFlightPage.java
+++ mercurytours/src/main/java/pages/BookAFlightPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 
 public class BookAFlightPage extends BasePage {
 
-	
-	
+	public BookAFlightPage(WebDriver driver) {
+        super(driver,driver.getCurrentUrl());
+        //All related sections will be here
+    }
+
 }
