be6ed70298c3af474000c7e1bf0fa5b61e80f5b9
Bran <brandock4@gmail.com>
2020-05-18 20:31:45 -0500

    Adding Attribures and methos to FlightFinderPage, FlightDetailsSection and DepartSection

--- mercurytours/src/main/java/sections/FlightDetailsSection.java
+++ mercurytours/src/main/java/sections/FlightDetailsSection.java
@@ -1,10 +1,59 @@
 package sections;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class FlightDetailsSection extends BaseSection{
 
+    private WebElement tripType;
+
+    private WebElement passengersCount;
+
+    private WebElement departingFrom;
+
+    private WebElement monthOn;
+
+    private WebElement dayOn;
+
+    private WebElement arrivingIn;
+
+    private WebElement monthReturn;
+
+    private WebElement dayReturn;
+
     public FlightDetailsSection(WebDriver driver){
         super(driver);
+    }
+
+    public void clickOnType(){
+
+    }
+
+    public void selectPassenger(){
+
+    }
+
+    public void selectDepartureFrom(){
+
+    }
+
+    public void selectMonthOn(){
+
+    }
+
+    public void selectDayOn(){
+
+    }
+
+    public void selectArriving(){
+
+    }
+
+    public void selectMonthReturn(){
+
+    }
+
+    public void selectDayReturn(){
+
     }
 }
