19961066f686068d841a62a6f93f4f0ef826a3ad
Francisco Galvan <jofagadaw@gmail.com>
2020-05-21 20:05:32 -0500

    TC03_HomePageLeftMenu allure comments added

--- mercurytours/src/test/java/testCasesMercury/HomePageLeftMenuTestCases.java
+++ mercurytours/src/test/java/testCasesMercury/HomePageLeftMenuTestCases.java
@@ -2,23 +2,31 @@ package testCasesMercury;
 
 import org.testng.annotations.Test;
 
+//REQ03 Home Page - Left Menu
+
 public class HomePageLeftMenuTestCases {
+
+    //Allure Description, please uncomment after add to the POM file
+    //@Description("TC12_Home Page - Left Menu_User is redirected to Flights Page")
 
     @Test(alwaysRun = true, priority = 0)
     public void validateUserLoggedRedirectedFlightFinderPage(){
 
     }
-
+    //Allure Description, please uncomment after add to the POM file
+    //@Description("TC13_Home Page - Left Menu_User is redirected to Cruises Page")
     @Test(alwaysRun = true, priority = 0)
     public void validateUserLoggedRedirectedCruiseSpecialPage(){
 
     }
-
+    //Allure Description, please uncomment after add to the POM file
+    //@Description("TC14_Home Page - Left MenuUser open Flights and Cruise at the same tame")
     @Test(alwaysRun = true, priority = 0)
     public void validateUserLoggedRedirectedAbleSeeAboutPages(){
 
     }
-
+    //Allure Description, please uncomment after add to the POM file
+    //@Description("TC15_Home Page - Left Menu_Prospect is redirected to Cruises Page")
     @Test(alwaysRun = true, priority = 0)
     public void validateProspectRedirectedCruiseSocialPage(){
 
