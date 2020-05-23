d3e20e5ecc9eb685ef39ce20de1d1d734d72fad2
Tony Cole <tonycol1984@gmail.com>
2020-05-23 11:37:00 -0500

    Add BaseTest, HomePageLeftMenuSignIn and HomePageTopMenuSpecialsSection test case classes

---
+++ mercurytours/src/test/java/testCasesMercury/HomePageLeftMenuSignIn.java
@@ -0,0 +1,56 @@
+package testCasesMercury;
+
+import org.testng.annotations.Test;
+
+public class HomePageLeftMenuSignIn {
+
+    @Test(alwaysRun = true, priority = 0)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC06_Sing-In_Validate 'Welcome Message'")*/
+    public void validateWelcomeMessage(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 1)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC07_Sing-In_Validate the 'Registration form' link")*/
+    public void validateRegistrationFormLink(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 2)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC07_Validate that the user can log-in with valid user and password")*/
+    public void validateValidLogin(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 3)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC08_Validate that the user can't log-in with invalid username and password")*/
+    public void validateInvalidLogin(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 4)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC09_Validate that 'User Name' field is mandatory)*/
+    public void validateUsernameIsMandatory(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 5)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC10_Validate that 'Password' field is mandatory)*/
+    public void validatePasswordIsMandatory(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 6)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC11_Validate copyright message)*/
+    public void validateCopyrightMessage(){
+
+    }
+
+}
---
+++ mercurytours/src/test/java/testCasesMercury/HomePageTopMenuSpecialsSection.java
@@ -0,0 +1,43 @@
+package testCasesMercury;
+
+import org.testng.annotations.Test;
+
+public class HomePageTopMenuSpecialsSection {
+
+
+    @Test(alwaysRun = true, priority = 0)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC16_Home Page - Top Menu_Specials Section_Validate 'Welcome Message'")*/
+    public void validateWelcomeMessage(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 1)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC17_Home Page - Top Menu_Specials Section_Validate 'Registration Message'")*/
+    public void validateRegistrationMessage(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 2)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC18_Home Page - Top Menu_Specials Section_Validate 'Under Construction Message' from Support Link")*/
+    public void validateSupportUnderConstructionMessage(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 3)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC19_Home Page - Top Menu_Specials Section_Validate 'Under Construction Message' from Contact Link")*/
+    public void validateContactUnderConstructionMessage(){
+
+    }
+
+    @Test(alwaysRun = true, priority = 4)
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC20_Home Page - Top Menu_Specials Section_Validate "Special Section"")*/
+    public void validateSpecialSection(){
+
+    }
+
+}
