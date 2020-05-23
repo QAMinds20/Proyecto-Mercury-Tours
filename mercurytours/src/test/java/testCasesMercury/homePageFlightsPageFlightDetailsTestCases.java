4f0ed2434d2b3e0d930c8c1256351d3275392e95
Edith1804 <edith.jimenezm@gmail.com>
2020-05-23 00:08:03 -0500

    Test case FlightFinder

---
+++ mercurytours/src/test/java/testCasesMercury/homePageFlightsPageFlightDetailsTestCases.java
@@ -0,0 +1,49 @@
+package testCasesMercury;
+
+import org.testng.annotations.Test;
+
+public class homePageFlightsPageFlightDetailsTestCases {
+
+    //@Description("TC21_Home Page - Flights Page_Flight Details_Default fields")
+    @Test(alwaysRun=true,priority=0)
+    public void verifyDefaultFields(){
+
+    }
+
+    // @Description("TC22_Home Page - Flights Page_Flight Details_Radio button")
+    @Test(alwaysRun=true,priority=1)
+    public void validateRadioButtonTrip(){
+
+    }
+
+    //@Description("TC23_Home Page - Flights Page_Flight Details_Passengers dropdown")
+    @Test(alwaysRun=true,priority=2)
+    public void  validatePessengerDropdown(){
+
+    }
+
+    //@Description("TC24_Home Page - Flights Page_Flight Details_Departing from dropdown")
+    @Test(alwaysRun=true,priority=3)
+    public void  validateDepartingDropdown(){
+
+    }
+
+    //@Description("TC25_Home Page - Flights Page_Flight Details_On' dropdowns")
+    @Test(alwaysRun=true,priority=4)
+    public void  validateOnMonthDay(){
+
+    }
+
+    //@Description("TC26_Home Page - Flights Page_Flight Details_Arriving In' dropdown")
+    @Test(alwaysRun=true,priority=5)
+    public void  validateArrivingDropdown(){
+
+    }
+
+    //@Description("Returning' dropdown")
+     @Test(alwaysRun=true,priority=6)
+    public void  (){
+
+    }
+
+}
