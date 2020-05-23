597184c13298c1b1a51cd07e753f3284de080f04
Edith1804 <edith.jimenezm@gmail.com>
2020-05-23 11:07:21 -0500

    Tes Cases Flight Finder V2

--- mercurytours/src/test/java/testCasesMercury/HomePageFlightsPageFlightDetailsTestCases.java
+++ mercurytours/src/test/java/testCasesMercury/HomePageFlightsPageFlightDetailsTestCases.java
@@ -2,7 +2,8 @@ package testCasesMercury;
 
 import org.testng.annotations.Test;
 
-public class homePageFlightsPageFlightDetailsTestCases {
+
+public class HomePageFlightsPageFlightDetailsTestCases {
 
     //@Description("TC21_Home Page - Flights Page_Flight Details_Default fields")
     @Test(alwaysRun=true,priority=0)
@@ -11,38 +12,38 @@ public class homePageFlightsPageFlightDetailsTestCases {
     }
 
     // @Description("TC22_Home Page - Flights Page_Flight Details_Radio button")
-    @Test(alwaysRun=true,priority=1)
+    @Test(alwaysRun=true,priority=0)
     public void validateRadioButtonTrip(){
 
     }
 
     //@Description("TC23_Home Page - Flights Page_Flight Details_Passengers dropdown")
-    @Test(alwaysRun=true,priority=2)
-    public void  validatePessengerDropdown(){
+    @Test(alwaysRun=true,priority=0)
+    public void  validatePassengerDropdown(){
 
     }
 
     //@Description("TC24_Home Page - Flights Page_Flight Details_Departing from dropdown")
-    @Test(alwaysRun=true,priority=3)
+    @Test(alwaysRun=true,priority=0)
     public void  validateDepartingDropdown(){
 
     }
 
     //@Description("TC25_Home Page - Flights Page_Flight Details_On' dropdowns")
-    @Test(alwaysRun=true,priority=4)
+    @Test(alwaysRun=true,priority=0)
     public void  validateOnMonthDay(){
 
     }
 
     //@Description("TC26_Home Page - Flights Page_Flight Details_Arriving In' dropdown")
-    @Test(alwaysRun=true,priority=5)
+    @Test(alwaysRun=true,priority=0)
     public void  validateArrivingDropdown(){
 
     }
 
     //@Description("Returning' dropdown")
-     @Test(alwaysRun=true,priority=6)
-    public void  (){
+     @Test(alwaysRun=true,priority=0)
+    public void returningDropdown (){
 
     }
 
