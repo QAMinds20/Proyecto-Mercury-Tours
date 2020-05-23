dbfe09a8e7fdf1157709cac28902049edbe34dbe
Armando Mora <satchmora@gmail.com>
2020-05-23 11:21:10 -0500

    PassengerSection update

--- mercurytours/src/main/java/sections/PassengerSection.java
+++ mercurytours/src/main/java/sections/PassengerSection.java
@@ -1,10 +1,39 @@
 package sections;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PassengerSection extends BaseSection{
 
+    private WebElement firstNameTxtB;
+    private WebElement lastNameTxtB;
+    private WebElement mealDropDown;
+
     public PassengerSection(WebDriver driver) {
         super(driver);
+    }
+
+    public void setName() {
+
+    }
+
+    public void setLastName() {
+
+    }
+
+    public void selectMeal() {
+
+    }
+
+    public void getName() {
+
+    }
+
+    public void getLastName(){
+
+    }
+
+    public void getMeal() {
+
     }
 }
