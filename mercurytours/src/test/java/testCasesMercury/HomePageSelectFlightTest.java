93f5aae0722361b1d81bb0727e5186a098e64a26
Sal Blumen <uribenski@gmail.com>
2020-05-25 19:32:25 -0500

    add Allure annotations, they are commented for future implementation

--- mercurytours/src/test/java/testCasesMercury/HomePageSelectFlightTest.java
+++ mercurytours/src/test/java/testCasesMercury/HomePageSelectFlightTest.java
@@ -4,19 +4,27 @@ import org.testng.annotations.Test;
 
 public class HomePageSelectFlightTest extends BaseTest{
 
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC31_Home Page - Select Flight_Only one option is allowed to select on Service Class section")*/
     @Test(alwaysRun=true, priority=0)
     public void validateOnlyOneOptionIsAllowedToSelectOnServiceClassSection(){
 
     }
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC32_Home Page - Select Flight_Only one option is allowed to select on Airline dropdown")*/
     @Test(alwaysRun=true, priority=0)
     public void validateOnlyOneOptionIsAllowedToSelectOnAirlineDropdown(){
 
     }
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC33_Home Page - Select Flight_Preferences default options")*/
     @Test(alwaysRun=true, priority=0)
     public void validatePreferencesDefaultOptions(){
 
     }
 
+    /*@Severity(SeverityLevel.BLOCKER)
+    @Description("TC34_Home Page - Select Flight_Select Continue button")*/
     @Test(alwaysRun=true, priority=0)
     public void  validateSelectContinueButton(){
 
