954247cd60f185fe6053ce417eb6c1cab38fa872
Francisco Galvan <jofagadaw@gmail.com>
2020-05-27 18:27:27 -0500

    Base section, TopMenuSection, Left Menu Section and Sign On methods updated

--- src/main/java/sections/LeftMenuSection.java
+++ src/main/java/sections/LeftMenuSection.java
@@ -1,7 +1,10 @@
 package sections;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LeftMenuSection extends BaseSection {
 
@@ -16,19 +19,30 @@ public class LeftMenuSection extends BaseSection {
 	}
 
 	public boolean isLoaded() {
-		return true;
+		try{
+			WebDriverWait wait = new WebDriverWait(this.driver, 5);
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//html/body/div/table/tbody/tr/td[1]/table/tbody/tr/td/table/tbody/tr/td/table")));
+			return true;
+		}
+		catch(Exception e){
+			return false;
+		}
 	}
 
 	public void clickOnFlights() {
-
+		flightsLink.findElement(By.xpath("//a[text()='Flights']"));
+		flightsLink.click();
 	}
 
 	public void clickOnCruises() {
-
+		cruisesLink.findElement(By.xpath("//a[text()='Cruises']"));
+		cruisesLink.click();
 	}
 
 	public void clickOnHome() {
-
+		homeLink.findElement(By.xpath("//a[text()='Home']"));
+		homeLink.click();
 	}
 
 }
+
